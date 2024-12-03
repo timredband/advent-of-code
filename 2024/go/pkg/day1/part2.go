62dbe9a00c34bcde477d82792e9437b23ad90b29
Tim Redband <tim.redband@dunnhumby.com>
2024-12-03 14:44:31 -0500

    2024 day 1 part 2

--- 2024/go/pkg/day1/part2.go
+++ 2024/go/pkg/day1/part2.go
@@ -1,7 +1,37 @@
 package day1
 
-import "os"
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+
+	"github.com/timredband/advent-of-code/pkg/utils"
+)
 
 func Part2(file *os.File) {
+	inputs := utils.ReadFile(file)
 
+	left := make([]int, 0, len(inputs))
+	right := make(map[int]int)
+
+	for _, value := range inputs {
+		r, _ := regexp.Compile("[0-9]+")
+		input := r.FindAllString(value, -1)
+
+		leftInt, _ := strconv.Atoi(input[0])
+		rightInt, _ := strconv.Atoi(input[1])
+
+		left = append(left, leftInt)
+		right[rightInt] += 1
+	}
+
+	result := 0
+
+	for i := 0; i < len(left); i += 1 {
+		similarity := left[i] * right[left[i]]
+		result += similarity
+	}
+
+	fmt.Println(result)
 }
