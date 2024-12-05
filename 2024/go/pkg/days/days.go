5dc2070916f5dcbc8bf66a7471374ab55c71cb23
Tim Redband <tim.redband@dunnhumby.com>
2024-12-05 14:48:37 -0500

    2024 day 4 part 1

--- 2024/go/pkg/days/days.go
+++ 2024/go/pkg/days/days.go
@@ -8,6 +8,7 @@ import (
 	"github.com/timredband/advent-of-code/pkg/day1"
 	"github.com/timredband/advent-of-code/pkg/day2"
 	"github.com/timredband/advent-of-code/pkg/day3"
+	"github.com/timredband/advent-of-code/pkg/day4"
 )
 
 func Execute(day string, part string, file *os.File) (int, error) {
@@ -18,6 +19,8 @@ func Execute(day string, part string, file *os.File) (int, error) {
 		return day2.Execute(part, file)
 	case "3":
 		return day3.Execute(part, file)
+	case "4":
+		return day4.Execute(part, file)
 	default:
 		return 0, errors.New(fmt.Sprintf("unknown day: %s", day))
 	}
