094d0e1e2a812227957d27285c9189c348033da2
timredband <timredband@gmail.com>
2025-12-05 16:05:24 -0500

    2025 day 1 part 2

--- 2025/rust/days/src/days/day01.rs
+++ 2025/rust/days/src/days/day01.rs
@@ -32,9 +32,44 @@ pub fn part1(file: &File) -> i64 {
 pub fn part2(file: &File) -> i64 {
     let reader = BufReader::new(file);
 
+    let mut result = 0;
+    let mut position: i32 = 50;
+
     for line in reader.lines() {
         let line = line.expect("line exists");
+        let direction = &line[0..1];
+        let clicks: i32 = (&line[1..]).parse().expect("valid integer");
+
+        if direction == "L" {
+            for i in 0..clicks {
+                position = position - 1;
+                position = position.rem_euclid(100);
+
+                if position == 0 {
+                    result += 1;
+                    let remaining = clicks - i - 1;
+                    result += remaining / 100;
+                    position = position - remaining;
+                    break;
+                }
+            }
+        }
+
+        if direction == "R" {
+            for i in 0..clicks {
+                position = position + 1;
+                position = position.rem_euclid(100);
+
+                if position == 0 {
+                    result += 1;
+                    let remaining = clicks - i - 1;
+                    result += remaining / 100;
+                    position = position + remaining;
+                    break;
+                }
+            }
+        }
     }
 
-    1
+    result.into()
 }
