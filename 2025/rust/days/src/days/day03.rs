7f192b5f7925fbdb1cc30402120849c372953bcc
timredband <timredband@gmail.com>
2025-12-06 12:36:13 -0500

    2025 day 3 part 2

--- 2025/rust/days/src/days/day03.rs
+++ 2025/rust/days/src/days/day03.rs
@@ -33,5 +33,42 @@ pub fn part1(file: &File) -> i64 {
 }
 
 pub fn part2(file: &File) -> i64 {
-    1
+    let reader = BufReader::new(file);
+
+    let mut result = 0;
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let length = line.len();
+        let mut matrix = vec![vec![0; length]; 12];
+
+        for j in (0..length).rev() {
+            for i in 0..matrix.len() {
+                if ((length as i64) - (j as i64) - (i as i64) - 1) < 0 {
+                    matrix[i][j] = -1;
+                }
+            }
+        }
+
+        for j in (0..length).rev() {
+            for i in 0..matrix.len() {
+                let mut current = line[j..j + 1].to_string();
+                let mut previous: i64 = -1;
+
+                if i != 0 && j + 1 < length && matrix[i - 1][j + 1] != -1 {
+                    current.push_str(&matrix[i - 1][j + 1].to_string());
+                }
+
+                if j + 1 < length {
+                    previous = matrix[i][j + 1];
+                }
+
+                matrix[i][j] = cmp::max(current.parse().unwrap(), previous);
+            }
+        }
+
+        result += matrix[11][0];
+    }
+
+    result
 }
