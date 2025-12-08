5187df8a49764ddf71f19cbc1cf20893f6c9a9c9
timredband <timredband@gmail.com>
2025-12-08 11:57:10 -0500

    2025 day 7 part 2

--- 2025/rust/days/src/days/day07.rs
+++ 2025/rust/days/src/days/day07.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::HashSet,
+    collections::{HashMap, HashSet},
     fs::File,
     io::{BufRead, BufReader},
 };
@@ -41,5 +41,41 @@ pub fn part1(file: &File) -> i64 {
 }
 
 pub fn part2(file: &File) -> i64 {
-    1
+    let reader = BufReader::new(file);
+
+    let mut beams: HashMap<usize, usize> = HashMap::new();
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+
+        // this is bad
+        if let Some(index) = line.find("S") {
+            beams.insert(index, 1);
+        }
+
+        let row_beams: Vec<usize> = beams.clone().into_keys().collect();
+
+        for beam in &row_beams {
+            if &line[*beam..*beam + 1] == "^" {
+                let timelines = beams[&beam];
+                beams.remove(beam);
+
+                if let Some(existing) = beams.get(&(beam - 1)) {
+                    beams.insert(beam - 1, existing + timelines);
+                } else {
+                    beams.insert(beam - 1, timelines);
+                }
+
+                if let Some(existing) = beams.get(&(beam + 1)) {
+                    beams.insert(beam + 1, existing + timelines);
+                } else {
+                    beams.insert(beam + 1, timelines);
+                }
+            }
+        }
+    }
+
+    let result = beams.into_values().fold(0, |acc, value| acc + value);
+
+    result.try_into().unwrap()
 }
