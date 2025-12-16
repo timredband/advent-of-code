d717f2b641d9c8b3bdc92fa09bd86403ccae5a15
timredband <timredband@gmail.com>
2025-12-16 15:08:47 -0500

    2025 day 8 part 2

--- 2025/rust/days/src/days/day08.rs
+++ 2025/rust/days/src/days/day08.rs
@@ -98,5 +98,78 @@ pub fn part1(file: &File) -> i64 {
 }
 
 pub fn part2(file: &File) -> i64 {
-    1
+    let reader = BufReader::new(file);
+    let re = Regex::new(r"(\d+),(\d+),(\d+)").unwrap();
+
+    let mut positions = Vec::new();
+    let mut circuits: Vec<HashSet<Position>> = Vec::new();
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+
+        let captures = re.captures(&line).unwrap();
+
+        let x: i64 = (&captures[1]).parse().unwrap();
+        let y: i64 = (&captures[2]).parse().unwrap();
+        let z: i64 = (&captures[3]).parse().unwrap();
+
+        let position = Position { x, y, z };
+        positions.push(position);
+
+        let mut set = HashSet::new();
+        set.insert(position);
+
+        circuits.push(set);
+    }
+
+    let mut connections = Vec::new();
+
+    for i in 0..positions.len() {
+        for j in i + 1..positions.len() {
+            connections.push(Connection::new(&positions[i], &positions[j]));
+        }
+    }
+
+    connections.sort_by(|a, b| a.distance.total_cmp(&b.distance));
+
+    let mut last = &connections[0];
+
+    for index in 0..connections.len() {
+        if circuits.len() == 1 && circuits[0].len() == positions.len() {
+            break;
+        }
+
+        let connection = &connections[index];
+        last = &connection;
+
+        let mut circuit = HashSet::new();
+        let mut indices: Vec<usize> = Vec::new();
+
+        for i in 0..circuits.len() {
+            if circuits[i].contains(connection.a) || circuits[i].contains(connection.b) {
+                circuits[i].insert(*connection.a);
+                circuits[i].insert(*connection.b);
+                circuit.extend(circuits[i].clone());
+                indices.push(i);
+            }
+        }
+
+        for i in (0..indices.len()).rev() {
+            circuits.swap_remove(indices[i]);
+        }
+
+        circuits.push(circuit);
+        circuits.retain(|circuit| circuit.len() != 0);
+    }
+
+    circuits.sort_by(|a, b| b.len().cmp(&a.len()));
+
+    // for circuit in &circuits {
+    //     println!("{} | {:?}", circuit.len(), circuit);
+    // }
+    //
+    // println!("{}", circuits.len());
+
+    let result = last.a.x * last.b.x;
+    result as i64
 }
